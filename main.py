035a01322c5f6c5aaa0964e0a1a58acde80c7fd0
anastasya2004 <62603182+anastasya2004@users.noreply.github.com>
2023-04-26 11:28:42 +0700

    2

--- main.py
+++ main.py
@@ -1,68 +1,30 @@
-Makeeva Angelina 85%
-Kareva Alena
-Osokina Nastya
-
-
-import datetime
-########
-
-def name_quest ():
-  print('Здравствуйте, как вас зовут?')
-  name = input()
-  print('Очень приятно,', name)
-
-def  money_in_russia_quest():
-  print('Вы получаете деньги в России?')
-  answer_mirq = input()
-  if answer_mirq.lower() == 'нет':
-      print('Хорошего дня')
-  return answer_mirq
-
-
-def live_in_russia_quest(answer_mirq):
-  cost = 0
-  if answer_mirq.lower() == "да":
-    print('На данный момент вы живёте в России?')
-    ans_1 = input()
-    if ans_1.lower() == 'да':
-      print('ok')
-    elif ans_1.lower() == 'нет':
-      current_date = datetime.datetime.now()
-      print('В какую дату вы последний раз были в России?')
-      day1, month1, year1 = map(int, input().split('.'))
-      lastday_date1 = datetime.datetime(year1, month1, day1)
-      need_days = datetime.timedelta(days = 183)
-      result = current_date - need_days
-      day = lastday_date1 - result
-      print ((current_date - lastday_date1).days)
-      if (current_date - lastday_date1).days > 183:
-        print('Вы отсутствовали из-за болезни, обучения, военной службы? ')
-        ans_2 = input()
-        if ans_2.lower() == 'да':
-         print('resident')
-        if ans_2.lower() == 'нет':
-          print('Вы \n 1)получаете доход по патенту на работу в РФ \n 2)высококвалифицированный специалист \n 3) переселившийся по госпрограмме в РФ \n 4)член экипажей российских судов \n 5)беженец \n 6)гражданин любой из стран ЕАЭС, которые работают по трудовому договру')
-          ans_3 = input()
-          if ans_3.lower() == 'да':
-            print('Сколько вы зарабатываете?')
-          zp = input()
-          cost += int(zp)
-          if ans_3.lower() == 'нет':
-            cost = cost + 0
-          print('Вы получаете дивиденты?')
-          ans_4 = input()
-          if ans_4.lower() == 'да':
-            cost = cost * 0.15
-          if ans_4.lower() == 'нет':
-            cost = cost * 0.30
-      print('Вы сдаете квартиру?')
-      ans_5 = input()
-      if ans_5.lower() == 'да':
-        print('Сколько стоит?')
-        ans_6 = input()
-        ans_6 = ans_6 * 0.3
-########
-
-name_quest()
-answer = money_in_russia_quest()
-live_in_russia_quest(answer)
+HELLO = 'Здравствуйте, как вас зовут?'
+NICE = 'Очень приятно,'
+MINR = 'Вы получаете деньги в России?'
+GOODDAY = 'Хорошего дня'
+LIVEINR = 'На данный момент вы живёте в России?'
+SELF = 'Вы являетесь самозанятым?'
+GIFTAD = 'На протяжении года у вас были выйгрыши в рекламных конкурсах?'
+SUM4 = 'Сумма выйгрыша составила более 4000 рублей?'
+WRITESUM = 'Напишите сумму вашего выйгрыша'
+NINAD = 'Налог с выйгрыша не платится'
+SECOND = 'Перейдем к следующему вопросу'
+REALTY = 'Вы продаете недвижимость?'
+NREALTY = 'Вы продаете жилую или нежилую недвижимость?'
+CTREALTY = 'За сколько вы продаете жилую недвижимость?'
+REALTYYEAR = 'Вы владеете нежилой недвижимостью более 3-х лет?'
+NCTREALTY = 'За сколько вы продаете нежилую недвижимость?'
+ZB = 'Вы владеете ценными бумагами?'
+SUMZB = 'Напишите сумму ваших ценных бумаг'
+RENT = 'Вы сдаете квартиру?'
+CTRENT = 'Сколько стоит?'
+ZP = 'Сколько вы зарабатываете в месяц?'
+NDFL = 'НДФЛ ='
+DATEINR = 'В какую дату вы последний раз были в России?'
+WHYABS = 'Вы отсутствовали из-за болезни, обучения, военной службы?'
+REASONS = 'Вы \n 1)получаете доход по патенту на работу в РФ \n 2)высококвалифицированный специалист \n 3) переселившийся по госпрограмме в РФ \n 4)член экипажей российских судов \n 5)беженец \n 6)гражданин любой из стран ЕАЭС, которые работают по трудовому договру'
+DIVIDEND = 'Вы получаете дивиденты?'
+YES = 'да'
+NO = 'нет'
+LIV = 'жилая'
+UNLIV = 'нежилая'
